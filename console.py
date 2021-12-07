1559974dc167184e58da2b09b1b01ba01157ec53
saimeerasingh <singhsaimeera@gmail.com>
2021-12-07 16:46:38 +0000

    CRUD added for staffs table

--- console.py
+++ console.py
@@ -1,3 +1,10 @@
 from models.staff import Staff
 import repositories.staff_repository as staff_repository
 
+staff_repository.delete_all()
+
+staff1 = Staff('Ada','08/08/2020','food',4)
+staff_repository.save(staff1)
+staff2 = Staff('Eve','09/10/2020','Health',5)
+staff_repository.save(staff2)
+
--- db/safari_park.sql
+++ db/safari_park.sql
@@ -3,5 +3,7 @@ DROP TABLE IF EXISTS staffs;
 CREATE TABLE staffs(
     id SERIAL PRIMARY KEY,
     name VARCHAR(255),
-    
+    start_date VARCHAR(255),
+    department VARCHAR(255),
+    performance_rating INT
 )
